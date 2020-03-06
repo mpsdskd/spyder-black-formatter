59426c804dd3328d9e8644666eb4726cc7a8c2ed
mpsdskd <mark_p._sanders@web.de>
2020-03-06 23:11:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -63,5 +63,6 @@ setup(
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
 )
